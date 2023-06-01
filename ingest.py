193e1dbe91db81714752b9bce08813ad8d6a334a
qiaochao <qiaochao@icourt.cc>
2023-06-01 16:16:01 +0800

    feat : process document

--- ingest.py
+++ ingest.py
@@ -5,8 +5,8 @@ from typing import List # 导入List类型注解，用于指定列表类型
 from dotenv import load_dotenv # 导入load_dotenv函数，用于加载环境变量
 from multiprocessing import Pool # 导入Pool类，用于创建进程池
 from tqdm import tqdm # 导入tqdm模块，用于显示进度条
-
-from langchain.document_loaders import ( # 从langchain.document_loaders模块导入以下类，用于加载不同格式的文档
+# 从langchain.document_loaders模块导入以下类，用于加载不同格式的文档
+from langchain.document_loaders import ( 
     CSVLoader,
     EverNoteLoader,
     PDFMinerLoader,
@@ -87,6 +87,7 @@ LOADER_MAPPING = { # 定义一个字典变量LOADER_MAPPING
 
 
 # 定义一个函数load_single_document，接受一个字符串类型的参数file_path，返回一个Document类型的对象
+# 该方法是下边load_document使用的方法
 def load_single_document(file_path: str) -> Document: 
      # 从file_path中获取文件扩展名，并在前面加上"."，赋给ext变量
     ext = "." + file_path.rsplit(".", 1)[-1]
@@ -94,46 +95,77 @@ def load_single_document(file_path: str) -> Document:
     if ext in LOADER_MAPPING: 
         # 从LOADER_MAPPING字典中获取对应的加载器类和参数，赋给loader_class和loader_args变量
         loader_class, loader_args = LOADER_MAPPING[ext] 
-        loader = loader_class(file_path, **loader_args) # 使用loader_class和loader_args创建一个加载器对象，赋给loader变量
-        return loader.load()[0] # 调用加载器的load方法，获取文档对象列表，并返回第一个元素
+         # 使用loader_class和loader_args创建一个加载器对象，赋给loader变量
+        loader = loader_class(file_path, **loader_args)
+         # 调用加载器的load方法，获取文档对象列表，并返回第一个元素
+        return loader.load()[0]
+    # 如果ext不在LOADER_MAPPING字典中，抛出一个ValueError异常，提示不支持的文件扩展名
+    raise ValueError(f"Unsupported file extension '{ext}'") 
 
-    raise ValueError(f"Unsupported file extension '{ext}'") # 如果ext不在LOADER_MAPPING字典中，抛出一个ValueError异常，提示不支持的文件扩展名
 
 
-
+# 定义一个函数load_documents，接受一个字符串类型的参数source_dir和一个字符串列表类型的参数ignored_files（默认为空列表），返回一个Document对象的列表
+# 函数的作用是从源文档目录中加载所有文档，忽略指定的文件
+# 这个方法下边的process_document会使用
 def load_documents(source_dir: str, ignored_files: List[str] = []) -> List[Document]:
-    """
-    Loads all documents from the source documents directory, ignoring specified files
-    """
+    # 定义一个空列表变量all_files，用于存储所有文件的路径
     all_files = []
+    # 遍历LOADER_MAPPING字典中的键（即文件扩展名）
     for ext in LOADER_MAPPING:
+        # 将以下列表添加到all_files列表中
         all_files.extend(
+            # 使用glob模块的glob函数，根据source_dir和扩展名拼接出一个通配符路径，然后递归地匹配所有符合条件的文件路径，返回一个列表
             glob.glob(os.path.join(source_dir, f"**/*{ext}"), recursive=True)
         )
+    # 使用列表推导式，从all_files列表中筛选出不在ignored_files列表中的文件路径，赋给filtered_files变量
     filtered_files = [file_path for file_path in all_files if file_path not in ignored_files]
 
+    # 使用Pool类创建一个进程池对象，进程数为os模块的cpu_count函数返回的值，赋给pool变量，并使用with语句管理其生命周期
     with Pool(processes=os.cpu_count()) as pool:
+        # 定义一个空列表变量results，用于存储文档对象
         results = []
+        # 使用tqdm模块的tqdm函数创建一个进度条对象，总数为filtered_files列表的长度，
+        # 描述为'Loading new documents'，列数为80，
+        # 赋给pbar变量，并使用with语句管理其生命周期
         with tqdm(total=len(filtered_files), desc='Loading new documents', ncols=80) as pbar:
+            # 使用pool对象的imap_unordered方法，对filtered_files列表中的每个元素（即文件路径）调用load_single_document函数，并返回一个迭代器；使用enumerate函数对迭代器进行编号；遍历编号和文档对象
             for i, doc in enumerate(pool.imap_unordered(load_single_document, filtered_files)):
+                # 将文档对象添加到results列表中
                 results.append(doc)
+                # 调用pbar对象的update方法，更新进度条
                 pbar.update()
 
+    # 返回results列表
     return results
 
+
+
+# 定义一个函数process_documents，
+# 接受一个字符串列表类型的参数ignored_files（默认为空列表），
+# 返回一个Document对象的列表
 def process_documents(ignored_files: List[str] = []) -> List[Document]:
-    """
-    Load documents and split in chunks
-    """
+    # 这个函数的作用是加载文档并拆分成片段
+    # 打印一条信息，显示正在从source_directory变量指定的目录中加载文档
     print(f"Loading documents from {source_directory}")
+    # 调用load_documents函数，传入source_directory和ignored_files参数，获取文档对象列表，赋给documents变量
     documents = load_documents(source_directory, ignored_files)
+    # 如果documents列表为空
     if not documents:
+        # 打印一条信息，显示没有新的文档可以加载
         print("No new documents to load")
+        # 退出程序
         exit(0)
+
+    
+    # 打印一条信息，显示从source_directory变量指定的目录中加载了多少个新的文档
     print(f"Loaded {len(documents)} new documents from {source_directory}")
+    # 使用RecursiveCharacterTextSplitter类创建一个文本拆分器对象，传入chunk_size和chunk_overlap参数，赋给text_splitter变量
     text_splitter = RecursiveCharacterTextSplitter(chunk_size=chunk_size, chunk_overlap=chunk_overlap)
+    # 调用text_splitter对象的split_documents方法，传入documents参数，获取拆分后的文本对象列表，赋给texts变量
     texts = text_splitter.split_documents(documents)
+    # 打印一条信息，显示拆分成了多少个文本片段，以及每个片段的最大长度（单位为token）
     print(f"Split into {len(texts)} chunks of text (max. {chunk_size} tokens each)")
+    # 返回texts列表
     return texts
 
 def does_vectorstore_exist(persist_directory: str) -> bool:
